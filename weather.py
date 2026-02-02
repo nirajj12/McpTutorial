2eb211468ae316606a998fcaba2795551b11a65c
nirajj12 <nirajofficial123@gmail.com>
2026-02-02 20:56:49 +0530

    weather tools added to the sever

---
+++ weather.py
@@ -0,0 +1,17 @@
+from mcp.server.fastmcp import FastMCP
+
+
+mcp=FastMCP("weather")
+
+
+@mcp.tool()
+async def get_weather(location:str)->str:
+    """Fetches the current weather for a given location.
+    
+    Args:
+        location (str): The location to get the weather for.
+    Returns:
+        str: A string describing the current weather.
+    """
+
+    return "The current weather in bangalore is rainy"
