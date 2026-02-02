34e4e7ea971bae0130fa038cbdc194a7a570108b
nirajj12 <nirajofficial123@gmail.com>
2026-02-02 20:54:01 +0530

    tools added to the sever updated

---
+++ calculator.py
@@ -0,0 +1,30 @@
+from mcp.server.fastmcp import FastMCP
+
+
+mcp=FastMCP("math")
+
+@mcp.tool()
+def add(a:int,b:int)->int:
+    """_summary"
+    
+    Args:
+        a (int): _description_
+        b (int): _description_
+
+    Returns:
+    int: _description_
+        """
+    return a+b
+
+@mcp.tool()
+def multiply(a:int,b:int)->int:
+    """_summary"
+    
+    Args:
+        a (int): _description_
+        b (int): _description_
+
+    Returns:
+    int: _description_
+        """
+    return a*b
